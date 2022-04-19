13420d3fe541caaa7aae6dd4225827f17e1eb4cd
daniyalinayat <daniyalinayatullah@gmail.com>
2022-04-19 13:37:38 +0200

    static build files

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'storyblok-translator-app',
+    target: 'static',
     htmlAttrs: {
       lang: 'en'
     },
