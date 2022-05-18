011065e527a69706c817ead4df8e37d0e6115ce6
jg <jg@montgomery-scott.virtual-identity.net>
2022-05-18 12:51:54 +0100

    cdk-backend added pipeline added-connected to github

---
+++ cdk-backend/lib/story-blok-pipeline-stack.ts
@@ -0,0 +1,49 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ManualApprovalStep,
+  ShellStep
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+//import { StoryBlokPipelineStage } from "./story-blok-stage";
+
+export class StoryBlokPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    //const pipeline =
+     new CodePipeline(this, "Pipeline", {
+      pipelineName: "StoryBlokPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("jp-guedes/vi-storyblok-deepl-translation-app", "deployment"),
+        commands: [
+          'cd cdk-backend',
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+        primaryOutputDirectory: "/cdk-backend/cdk.out"
+      })
+    });
+
+
+/*
+    const testingStage = pipeline.addStage(
+      new StoryBlokPipelineStage(this, "test", {
+        env: { account: "113510251150", region: "eu-central-1" }
+      })
+    );
+
+    testingStage.addPost(
+      new ManualApprovalStep("Manual Approval before production")
+    );
+
+    const prodStage = pipeline.addStage(
+      new StoryBlokPipelineStage(this, "prod", {
+        env: { account: "113510251150", region: "eu-central-1" }
+      })
+    );
+    */
+  }
+}
---
+++ cdk-backend/lib/story-blok-stage.ts
@@ -0,0 +1,24 @@
+/*
+import { CfnOutput, Stage, StageProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { StoryBlokStack } from "./story-blok-stack";
+
+export class StoryBlokPipelineStage extends Stage {
+  public readonly urlOutput: CfnOutput;
+
+  constructor(
+    scope: Construct,
+    stageName: string,
+    props?: StageProps
+  ) {
+    super(scope, stageName, props);
+
+    const storyBlokStack = new StoryBlokStack(
+      this,
+      "StoryBlokStack",
+      stageName
+    );
+  }
+}
+
+*/
