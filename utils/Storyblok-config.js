a7f0b89a2e3dd9dfaa0823a1bbd80ccb9ff6ec09
daniyalinayat <daniyal.inayatullah@virtual-identity.com>
2022-07-19 00:09:27 +0200

    test: access token uncommented

--- utils/Storyblok-config.js
+++ utils/Storyblok-config.js
@@ -3,7 +3,7 @@ import StoryblokClient from "storyblok-js-client";
 // 2. Initialize the client with the preview token
 // from your space dashboard at https://app.storyblok.com
 let Storyblok = new StoryblokClient({
-	// accessToken: process.env.ACCESS_TOKEN, //Sample Space
+	accessToken: process.env.ACCESS_TOKEN, //Sample Space
 	oauthToken: "oEUkARZp6T0hsoGu5MzQoAtt-93801-ThmkXinfFu3HiGeYzNku",
 });
 
